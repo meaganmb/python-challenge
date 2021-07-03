7e36e90ce4fac7ccf1fe07e18b7fd73938a6a221
Meagan Murray-Bruce <meaganmb_11@Meagans-MacBook-Pro.local>
2021-07-02 19:24:53 -0700

    forgot f string, completed pybank homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,11 +22,8 @@ with open(csvfile) as csv_file:
         counter += 1
         current_values = current_values + int(row[1])
         months.append(row[0])
-        # print(counter)
-        # print(current_values)
-        # print(months)
 
-        if row[0] == "Jan-2010":
+        if row[0] == months[0]:
             previous_values = (int(row[1]))
         else:
             diff_values.append(int(row[1]) - previous_values)
@@ -34,26 +31,19 @@ with open(csvfile) as csv_file:
         previous_values = (int(row[1]))
 
         # print(diff_values)
-
+    # Calculating average, minimum profit and maximum profit
     average = sum(diff_values) / (len(months) - 1)
     round_average = round(average, 2)
-    print(round_average)
     min_diff = min(diff_values)
     max_diff = max(diff_values)
-    print(min_diff)
-    print(max_diff)
 
     # find month of greatest decrease
     min_index = diff_values.index(min_diff)
-    print(min_index)
     min_month = months[(min_index + 1)]
-    print(min_month)
 
     # find month of greatest increase
     max_index = diff_values.index(max_diff)
-    print(max_index)
     max_month = months[(max_index + 1)]
-    print(max_month)
 
 
 # open file in w (write) mode
@@ -61,7 +51,7 @@ with open(textfile, "w") as text_file:
     text_file.write("Financial Analysis\n"
                     "----------------------------\n"
                     f"Total Months: {counter}\n"
-                    "Total: {current_values}\n"
-                    "Average Change: ${round_average}\n"
-                    "Greatest Increase in Profits: {max_month} (${max_diff})\n"
-                    "Greatest Decrease in Profits: {min_month} (${min_diff})\n")
+                    f"Total: {current_values}\n"
+                    f"Average Change: ${round_average}\n"
+                    f"Greatest Increase in Profits: {max_month} (${max_diff})\n"
+                    f"Greatest Decrease in Profits: {min_month} (${min_diff})\n")
